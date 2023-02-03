f72c7c7b6d086e47a582d1d00f95d69bd6383eef
zekenaulty@gmail.com <zekenaulty@gmail.com>
2023-02-02 23:28:27 -0600

    Added stage

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-console.log('app boot');
-
 import { go } from './isReady.js';
 import { Header } from './layout/header/header.js';
+import { Stage } from './layout/stage/stage.js';
 
 
 (() => {
@@ -9,6 +8,13 @@ import { Header } from './layout/header/header.js';
   go(() => {
 
     const header = new Header();
+    const stage = new Stage((gfx) => {
+        gfx.beginPath();
+        gfx.rect(10, 10, 100, 100);
+        gfx.fillStyle = 'red';
+        gfx.fill();
+        gfx.closePath();
+      });
 
     header.addButton('SAVE', (e) => {});
     header.addButton('NEW GAME', (e) => {});
---
+++ layout/stage/stage.js
@@ -0,0 +1,54 @@
+import { go } from '../../isReady.js';
+
+export class Stage {
+
+  #styles;
+  #stage;
+  #canvas;
+  #gfx;
+
+  constructor(ready) {
+    
+    this.onReady = ready;
+    
+    this.#styles = document.createElement('link');
+
+    this.#styles.rel = 'stylesheet';
+    this.#styles.href = './layout/stage/stage.css';
+
+    document.querySelector('head').appendChild(this.#styles);
+
+    this.#stage = document.createElement('div');
+    this.#stage.classList.add('stage');
+
+    document.querySelector('body').appendChild(this.#stage);
+
+    this.#canvas = document.createElement('canvas');
+    this.#canvas.classList.add('viewport');
+    this.#stage.appendChild(this.#canvas);
+
+    this.#gfx = this.#canvas.getContext("2d");
+
+    this.#scale();
+
+  }
+
+  get gfx() {
+    return this.#gfx;
+  }
+
+  #scale() {
+    this.#canvas.width = this.#stage.offsetWidth;
+    this.#canvas.height = this.#stage.offsetHeight;
+    if (this.#canvas.height > 200) {
+      this.onReady(this.#gfx);
+    } else {
+      setTimeout(() => { this.#scale(); }, 1);
+    }
+  }
+  
+  onReady(gfx) {
+
+  }
+
+}
