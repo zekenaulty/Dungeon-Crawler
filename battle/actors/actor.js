b0faba35b08f8e724af77296733a9e2acec044ef
zekenaulty@gmail.com <zekenaulty@gmail.com>
2023-02-07 01:45:52 -0600

    Added basic (bad) scaling rewards based on level. I really need internet... Implemented basic skill system, with Cds and Gcd. Planning to not do turn based battle and just use setInterval to run enimies "AI", we'll see how it turns out. No cast times or interupts yet, but that should be possible.

--- battle/actors/actor.js
+++ battle/actors/actor.js
@@ -16,10 +16,20 @@ export class ActorAttributes extends EventHandler {
   hp;
   mp;
 
+  available = 0;
+  pointsPerLevel = 5;
+
   constructor(actor) {
     super();
 
+    let self = this;
+
     this.#actor = actor;
+    this.#actor.listenToEvent('constructed', () => {
+      actor.listenToEvent('leveled up', () => {
+        self.available += self.pointsPerLevel;
+      });
+    });
 
     this.hp = this.maxHp;
     this.mp = this.maxMp;
@@ -40,20 +50,45 @@ export class ActorAttributes extends EventHandler {
   get maxDamage() {
     return this.baseDamage + Math.floor(this.strength / 3);
   }
-}
+} /* end ActorAttributes */
+
+
+
+
 
 export class ActorLevel extends EventHandler {
 
   #actor;
 
   level = 1;
-  baseXpToLevel = 100;
-  xpLevelFactor = 0.25;
-  xpToLevel = 500;
+  toLevelXp = 500;
   xp = 0;
 
-  static monsterXp(level = 1, factor = 0.05, cap = 1000, base = 50) {
+  static xpForNextLevel(
+    level = 1,
+    toLevelXp = 500,
+    xpFactor = 0.25,
+    baseXpToLevel = 100
+  ) {
+    return baseXpToLevel * level + Math.floor(toLevelXp * xpFactor);
+  }
 
+  static monsterXp(
+    level = 1,
+    factor = 0.05,
+    base = 10,
+    cap = 1000
+  ) {
+    let amount = 0;
+    for (let l = 1; l <= level; l++) {
+      if (amount >= cap) {
+        break;
+      }
+      amount +=
+        Math.ceil(base * (this.level / 2)) +
+        Math.floor(amount * factor);
+    }
+    return amount;
   }
 
   constructor(actor) {
@@ -64,11 +99,37 @@ export class ActorLevel extends EventHandler {
     this.defineEvent(
       'leveled up',
       'gained xp',
-      'lost xp');
-
+      'lost xp'
+    );
   }
 
-}
+  addXp(amount) {
+    this.xp += amount;
+    while (this.xp >= this.toLevelXp) {
+      this.levelUp();
+    }
+    this.raiseEvent('gained xp');
+  }
+
+  levelUp() {
+    let level = this.level;
+    this.level++;
+
+    this.toLevelXp += ActorLevel
+      .xpForNextLevel(
+        this.level,
+        this.toLevelXp,
+        this.xpFactor,
+        this.baseXpToLevel
+      );
+
+    this.raiseEvent('leveled up');
+  }
+} /* end ActorLevel */
+
+
+
+
 
 export class ActorInventory extends EventHandler {
 
@@ -81,8 +142,21 @@ export class ActorInventory extends EventHandler {
     return 40;
   }
 
-  static monsterGold(level = 1, factor = 0.05, cap = 100, base = 10) {
-
+  static monsterGold(
+    level = 1,
+    factor = 0.05,
+    base = 10,
+    cap = 100) {
+    let amount = 0;
+    for (let l = 1; l <= level; l++) {
+      if (amount >= cap) {
+        break;
+      }
+      amount +=
+        Math.ceil(base * (this.level / 2)) +
+        Math.floor(amount * factor);
+    }
+    return amount;
   }
 
   constructor(actor) {
@@ -93,36 +167,189 @@ export class ActorInventory extends EventHandler {
     this.defineEvent(
       'inventory full',
       'item added',
-      'item dropped');
-
+      'item dropped',
+      'gold added',
+      'gold lost'
+    );
   }
 
   addItem(item) {
-    if(this.items.length === this.maxItemSlots - 1) {
+    if (this.items.length === this.maxItemSlots - 1) {
       this.raiseEvent('inventory full', item);
     }
   }
 
-}
+  addGold(amount) {
+    this.gold += amount;
+    this.raiseEvent('gold added', amount);
+  }
+
+  loseGold(amount) {
+    this.gold -= amount;
+    this.raiseEvent('gold lost', amount);
+  }
+
+} /* end ActorInventory */
+
+
+
+
 
 export class Actor extends EventHandler {
 
   attributes;
   level;
   inventory;
+  skills = {};
+  target;
 
   constructor() {
     super();
 
-    this.attributes = new ActorAttributes(this);
     this.level = new ActorLevel(this);
+    this.attributes = new ActorAttributes(this);
     this.inventory = new ActorLevel(this);
 
     this.defineEvent(
+      'constructed',
       'leveled up',
-      'health changed',
-      'death');
+      'healed',
+      'damaged',
+      'death'
+    );
 
+    this.addSkill('GCD', new GCD(this));
+    this.addSkill('attack', new BasicAttack(this));
+
+    this.raiseEvent('constructed');
+  }
+
+  takeDamage(dmg) {
+    this.attributes.hp -= dmg;
+    this.raiseEvent('damaged', dmg);
+    if (this.attributes.hp < 1) {
+      this.raiseEvent('death');
+    }
+  }
+
+  addSkill(key, skill) {
+    this.skills[key] = skill;
+  }
+
+
+} /* end Actor */
+
+
+export class ActorSkill extends EventHandler {
+  actor;
+  cooldown = 1500;
+  lastUsed = 0;
+  minBy = 0;
+  maxBy = 0;
+  lock = false;
+  register = false;
+
+
+  get now() {
+    return new Date().getTime();
+  }
+
+  get onCd() {
+    return this.now - this.lastUsed < this.cooldown;
+  }
+
+  get onGcd() {
+    if (!this.actor.skills['GCD']) {
+      return false;
+    }
+    return this.actor.skills['GCD'].onCd;
+  }
+
+  gcd() {
+    if (!this.actor.skills['GCD']) {
+      return;
+    }
+    this.actor.skills['GCD'].invoke();
+  }
+
+  constructor(actor) {
+    super();
+
+    this.actor = actor;
+
+    this.resetCooldown;
+
+  }
+
+
+  doAttack(target) {
+    let maxDmg = this.actor.attributes.maxDamage + this.maxBy;
+    let minDmg = this.actor.attributes.minDamage + this.minBy;
+    let dmg = Math.ceil(Math.random() * maxDmg) + 1;
+    if (dmg < minDmg) {
+      dmg = minDmg + 1;
+    } else if (dmg > maxDmg) {
+      dmg = maxDmg;
+    }
+
+    //TODO use dex for crit and dodge
+    //TODO add chance to miss
+
+    target.takeDamage(dmg);
+  }
+
+  invoke() {
+
+  }
+
+  resetCooldown() {
+    this.lastUsed = this.now - this.cooldown;
+  }
+
+  refresh() {}
+
+  safeInvoke(action) {
+    if (this.lock || this.onGcd) {
+      return;
+    }
+
+    this.lock = true;
+    if (!this.onCd && this.actor.attributes.hp >= 1) {
+      action();
+      this.lastUsed = this.now;
+    }
+    this.lock = false;
+
+  }
+
+}
+
+
+class GCD extends ActorSkill {
+
+  constructor(actor) {
+    super(actor);
+    this.cooldown = 250;
+  }
+
+  invoke() {
+    safeInvoke(() => { /* GCD DO NOTHING */ });
+  }
+
+}
+
+class BasicAttack extends ActorSkill {
+
+  constructor(actor) {
+    super(actor);
+    this.cooldown = 1000;
+    this.register = true;
+  }
+
+  invoke(target) {
+    safeInvoke(() => {
+      this.doAttack(target);
+    });
   }
 
 }
