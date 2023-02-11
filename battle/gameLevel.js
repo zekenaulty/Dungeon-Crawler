dfd77a008f9c8b8402235016c5781ebbdf0db485
zekenaulty@gmail.com <zekenaulty@gmail.com>
2023-02-10 20:54:37 -0600

    bug fix

--- battle/gameLevel.js
+++ battle/gameLevel.js
@@ -179,7 +179,7 @@ export class GameLevel extends EventHandler {
     vm.#mazeMaxRooms += Math.ceil(vm.#mazeMaxRooms * vm.#roomGrowthFactor);
     vm.#resetMaze();
     vm.#randomMaze();
-    vm.#hero.restore();
+    vm.#hero.recover();
   }
 
   #resetMaze() {
